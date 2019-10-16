e9a4542ff464f5d6dbf876c48b6d54e5bf4c8816
YairTawil <yairtawil@gmail.com>
2019-10-16 21:07:09 +0300

    Auth component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.css'
 import { Route, Switch } from 'react-router'
 import PrivateRoute from './components/PrivateRoute'
 import Home from './components/Home/Home'
 import Welcome from './components/Welcome'
-import { setUser } from './state/actions/auth'
-import { auth, db, messaging } from './firebase'
-import { useDispatch } from 'react-redux'
+import { Auth } from './Auth'
 
-const App = () => {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    function handleTokenRefresh() {
-      return messaging.getToken().then((token) => {
-        console.log('token', token)
-        db.collection('users')
-          .doc(auth.currentUser.uid)
-          .set({ token }, { merge: true })
-      })
-    }
-
-    function onAuthStateChanged(user) {
-      if (user) {
-        const { uid: id, displayName, photoURL, email, phoneNumber } = user
-        db.collection('users').doc(user.uid).set({ id, displayName, photoURL, email, phoneNumber })
-
-        Notification.requestPermission()
-          .then(handleTokenRefresh)
-          .catch((err) => console.log('error getting permission :(', err))
-
-        messaging.onTokenRefresh(handleTokenRefresh)
-
-        messaging.onMessage((what) => {
-          console.log('onMessage!!!!!!!!!!!!!!!!!!', what)
-        })
-      }
-
-      dispatch(setUser(user))
-    }
-
-    auth.onAuthStateChanged(onAuthStateChanged)
-  }, [dispatch])
-
-  return (
-    <Switch>
+const App = () => (
+  <Switch>
+    <Auth>
       <Route path="/welcome" component={Welcome} />
       <PrivateRoute path="/" component={Home} />
-    </Switch>
-  )
-}
+    </Auth>
+  </Switch>
+)
 
 export default App
